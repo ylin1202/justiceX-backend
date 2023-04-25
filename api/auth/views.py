9bafb767b565e63dcd330a685e49aaaa28104690
hsinyilin <985471zxv@gmail.com>
2023-04-25 15:51:48 +0800

    add login request

--- api/auth/views.py
+++ api/auth/views.py
@@ -4,8 +4,12 @@ import string
 from email.mime.text import MIMEText
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from api.auth.serializers import *
 
 from api.models import *
@@ -33,6 +37,31 @@ def register(request):
         'success': True,
         'message': '註冊成功'
     }, status=status.HTTP_201_CREATED)
+
+
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
-    pass
+    data = request.data
+
+    sha256 = hashlib.sha256()
+    sha256.update(data['password'].encode('utf-8'))
+    password_hash = sha256.hexdigest()
+
+    user = Account.objects.filter(email=data['email'], password=password_hash)
+    if not user.exists():
+        return Response({
+            'success': False,
+            'message': '帳號或密碼錯誤'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    user = user.first()
+
+    refresh = RefreshToken.for_user(user)
+    access_token = str(refresh.access_token)
+
+    return Response({
+        'success': True,
+        'access_token': access_token,
+    })
+
--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'api.apps.ApiConfig',
 ]
 
@@ -85,6 +87,24 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'USER_ID_FIELD': 'email',
+    'PAYLOAD_ID_FIELD': 'email',
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
