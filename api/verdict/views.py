6c1a0008582fd5fef176d1497bd19543869cd108
hsinyilin <985471zxv@gmail.com>
2023-04-26 16:02:25 +0800

    update get_verdict_content request

--- api/verdict/views.py
+++ api/verdict/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.models import Verdict
+from api.models import *
 
 
 @api_view(['GET'])
@@ -9,12 +9,31 @@ def get_verdict_content(request):
     data = request.query_params
 
     verdict_id = data.get('verdict_id')
+    # user_id = request.user_id
+
     verdict = Verdict.objects.get(id=verdict_id)
+    total_like = Like.objects.filter(verdict_id=verdict_id).count()
+    total_comment = Comment.objects.filter(verdict_id=verdict_id).count()
+
+    # recommendations
+    # ....
 
     return Response({
         'success': True,
         'data': {
-            'id': verdict.id,
+            'verdict_id': verdict.id,
             'title': verdict.title,
+            'sub_title': verdict.sub_title,
+            'ver_title': verdict.ver_title,
+            'judgement_date': verdict.judgement_date,
+            'result': verdict.result,
+            'laws': verdict.laws,
+            'url': verdict.url,
+            'crime_id': verdict.crime.id,
+            'crime_type': verdict.crime.name,
+            'total_like': total_like,
+            'total_comment': total_comment,
+            'create_time': verdict.create_time,
+            'recommendations': []
         }
     })
