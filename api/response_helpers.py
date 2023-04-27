95f3fb071aae21f10d5db8507f7501f6b0aba153
hsinyilin <985471zxv@gmail.com>
2023-04-27 11:44:59 +0800

    update success_response default message

--- api/response_helpers.py
+++ api/response_helpers.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-def success_response(data=None, message='成功', status_code=status.HTTP_200_OK):
+def success_response(data=None, message=None, status_code=status.HTTP_200_OK):
     response_data = {'success': True}
     if data is not None:
         response_data['data'] = data
