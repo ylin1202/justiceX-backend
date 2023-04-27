80db0f2ea73db51c099ccb1364fd7c12fad90f47
hsinyilin <985471zxv@gmail.com>
2023-04-27 10:56:48 +0800

    refactor verdict/views.py response

--- api/verdict/views.py
+++ api/verdict/views.py
@@ -1,9 +1,9 @@
 from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 from api.models import *
+from api.response_helpers import *
 
 
 @api_view(['GET'])
@@ -20,47 +20,45 @@ def get_verdict(request):
     # recommendations
     # ....
 
-    return Response({
-        'success': True,
-        'data': {
-            'verdict_id': verdict.id,
-            'title': verdict.title,
-            'sub_title': verdict.sub_title,
-            'ver_title': verdict.ver_title,
-            'judgement_date': verdict.judgement_date,
-            'incident': verdict.incident,
-            'result': verdict.result,
-            'laws': verdict.laws,
-            'url': verdict.url,
-            'crime_id': verdict.crime.id,
-            'crime_type': verdict.crime.name,
-            'total_like': total_like,
-            'total_comment': total_comment,
-            'create_time': verdict.create_time,
-            'recommendations': []
-        }
-    })
+    data = {
+        'verdict_id': verdict.id,
+        'title': verdict.title,
+        'sub_title': verdict.sub_title,
+        'ver_title': verdict.ver_title,
+        'judgement_date': verdict.judgement_date,
+        'incident': verdict.incident,
+        'result': verdict.result,
+        'laws': verdict.laws,
+        'url': verdict.url,
+        'crime_id': verdict.crime.id,
+        'crime_type': verdict.crime.name,
+        'total_like': total_like,
+        'total_comment': total_comment,
+        'create_time': verdict.create_time,
+        'recommendations': []
+    }
+
+    return success_response(data=data)
 
 
 @api_view(['GET'])
 def get_verdicts(request):
     verdicts = Verdict.objects.all()
 
-    return Response({
-        'success': True,
-        'data': [
-            {
-                'verdict_id': verdict.id,
-                'title': verdict.title,
-                'judgement_date': verdict.judgement_date,
-                'total_like': Like.objects.filter(verdict_id=verdict.id).count(),
-                'total_comment': Comment.objects.filter(verdict_id=verdict.id).count(),
-                'crime_id': verdict.crime.id,
-                'crime_type': verdict.crime.name
-            }
-            for verdict in verdicts
-        ]
-    })
+    data = [
+        {
+            'verdict_id': verdict.id,
+            'title': verdict.title,
+            'judgement_date': verdict.judgement_date,
+            'total_like': Like.objects.filter(verdict_id=verdict.id).count(),
+            'total_comment': Comment.objects.filter(verdict_id=verdict.id).count(),
+            'crime_id': verdict.crime.id,
+            'crime_type': verdict.crime.name
+        }
+        for verdict in verdicts
+    ]
+
+    return success_response(data=data)
 
 
 @api_view(['GET'])
@@ -72,21 +70,20 @@ def filter_verdicts(request):
 
     verdicts = Verdict.objects.filter(title__icontains=title).order_by('-judgement_date')
 
-    return Response({
-        'success': True,
-        'data': [
-            {
-                'verdict_id': verdict.id,
-                'title': verdict.title,
-                'judgement_date': verdict.judgement_date,
-                'total_like': Like.objects.filter(verdict_id=verdict.id).count(),
-                'total_comment': Comment.objects.filter(verdict_id=verdict.id).count(),
-                'crime_id': verdict.crime.id,
-                'crime_type': verdict.crime.name
-            }
-            for verdict in verdicts
-        ]
-    })
+    data = [
+        {
+            'verdict_id': verdict.id,
+            'title': verdict.title,
+            'judgement_date': verdict.judgement_date,
+            'total_like': Like.objects.filter(verdict_id=verdict.id).count(),
+            'total_comment': Comment.objects.filter(verdict_id=verdict.id).count(),
+            'crime_id': verdict.crime.id,
+            'crime_type': verdict.crime.name
+        }
+        for verdict in verdicts
+    ]
+
+    return success_response(data=data)
 
 
 @api_view(['GET'])
@@ -97,21 +94,20 @@ def get_crime_verdicts(request):
 
     verdicts = Verdict.objects.filter(crime_id=crime_id)
 
-    return Response({
-        'success': True,
-        'data': [
-            {
-                'verdict_id': verdict.id,
-                'title': verdict.title,
-                'judgement_date': verdict.judgement_date,
-                'total_like': Like.objects.filter(verdict_id=verdict.id).count(),
-                'total_comment': Comment.objects.filter(verdict_id=verdict.id).count(),
-                'crime_id': verdict.crime.id,
-                'crime_type': verdict.crime.name
-            }
-            for verdict in verdicts
-        ]
-    })
+    data = [
+        {
+            'verdict_id': verdict.id,
+            'title': verdict.title,
+            'judgement_date': verdict.judgement_date,
+            'total_like': Like.objects.filter(verdict_id=verdict.id).count(),
+            'total_comment': Comment.objects.filter(verdict_id=verdict.id).count(),
+            'crime_id': verdict.crime.id,
+            'crime_type': verdict.crime.name
+        }
+        for verdict in verdicts
+    ]
+
+    return success_response(data=data)
 
 
 @api_view(['POST'])
@@ -124,15 +120,9 @@ def like_verdict(request):
     try:
         Like.objects.create(verdict_id=verdict_id, email_id=email)
     except IntegrityError:
-        return Response({
-            'success': False,
-            'message': '已按讚過'
-        }, status=status.HTTP_409_CONFLICT)
+        return error_response(message='已按過讚', status_code=status.HTTP_409_CONFLICT)
 
-    return Response({
-        'success': True,
-        'message': '成功'
-    }, status=status.HTTP_201_CREATED)
+    return success_response(status_code=status.HTTP_201_CREATED)
 
 
 @api_view(['DELETE'])
@@ -145,16 +135,8 @@ def unlike_verdict(request):
     like = Like.objects.filter(verdict_id=verdict_id, email_id=email)
 
     if not like.exists():
-        return Response({
-            'success': False,
-            'message': '已收回讚'
-        }, status=status.HTTP_410_GONE)
+        return error_response(message='已收回讚', status_code=status.HTTP_410_GONE)
 
     like.delete()
 
-    return Response({
-        'success': True,
-        'message': '成功'
-    }, status=status.HTTP_200_OK)
-
-
+    return success_response()
