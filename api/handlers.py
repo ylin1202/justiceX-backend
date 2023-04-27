5a0325695baf1cae96e0372e9b6e62404d9065e4
hsinyilin <985471zxv@gmail.com>
2023-04-27 09:37:42 +0800

    update handlers.py

--- api/handlers.py
+++ api/handlers.py
@@ -1,15 +1,15 @@
 from rest_framework.views import exception_handler
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
 from rest_framework_simplejwt.exceptions import InvalidToken
 from rest_framework.response import Response
 
 
 def custom_exception_handler(exc, context):
     if isinstance(exc, InvalidToken):
-        return Response({'success': False, 'message': 'token錯誤'}, status=401)
+        print({'success': False, 'message': '無效的token'})
     elif isinstance(exc, AuthenticationFailed):
-        return Response({'success': False, 'message': '權限驗證錯誤'}, status=401)
+        print({'success': False, 'message': '權限錯誤'})
+    elif isinstance(exc, NotAuthenticated):
+        print({'success': False, 'message': 'token遺失'})
 
-    response = exception_handler(exc, context)
-
-    return response
+    return Response({'success': False, 'message': '請重新登入'}, status=401)
