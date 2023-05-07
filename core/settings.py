92c3f065038fcb21fc2521163f0c7778c6a1975e
Boto <11136016@ntub.edu.tw>
2023-05-07 23:52:05 +0800

    update settings

--- core/settings.py
+++ core/settings.py
@@ -24,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '***REMOVED***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['13.208.146.112',
-                 'localhost',
-                 '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
