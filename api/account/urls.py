a22d35c2de6940c9a7e5be43423a12c5199603ea
Boto <11136016@ntub.edu.tw>
2023-05-20 00:10:25 +0800

    Add change_password & get_account & edit_account

--- api/account/urls.py
+++ api/account/urls.py
@@ -3,4 +3,7 @@ from api.account.views import *
 
 urlpatterns = [
     path('picture_list/', picture_list, name='picture_list'),
+    path('get_account/', get_account, name='get_account'),
+    path('edit_account/', edit_account, name='edit_account'),
+    path('change_password/', change_password, name='change_password')
 ]
--- api/account/views.py
+++ api/account/views.py
@@ -1,7 +1,9 @@
+import hashlib
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-from api.models import Picture
+from api.models import *
 from api.response_helpers import success_response
 
 
@@ -19,3 +21,61 @@ def picture_list(request):
     ]
 
     return success_response(data=data)
+
+
+@api_view(['GET'])
+def get_account(request):
+    data = request.data
+    email = request.user_id
+
+    acc = Account.objects.get(email=email)
+
+    data = {
+        'email': acc.email,
+        'name': acc.name,
+        'password': acc.password,
+        'gender': acc.gender,
+        'job_id': acc.job.id,
+        'job_name': acc.job.name,
+        'picture_id': acc.picture.id,
+        'picture': acc.picture.photo,
+        'is_notification': acc.is_notification,
+        'is_quiz': acc.is_quiz
+    }
+    return success_response(data=data)
+
+
+@api_view(['PATCH'])
+def edit_account(request):
+    data = request.data
+    email = request.user_id
+    name = data.get('name')
+    gender = data.get('gender')
+    job_id = data.get('job_id')
+    picture_id = data.get('picture_id')
+
+    acc = Account.objects.get(email=email)
+    acc.name = name
+    acc.gender = gender
+    acc.job_id = job_id
+    acc.picture_id = picture_id
+    acc.save()
+
+    return success_response(message='成功')
+
+
+@api_view(['PATCH'])
+def change_password(request):
+    data = request.data
+    email = request.user_id
+
+    acc = Account.objects.get(email=email)
+
+    sha256 = hashlib.sha256()
+    sha256.update(data['password'].encode('utf-8'))
+    password_hash = sha256.hexdigest()
+    acc.password = password_hash
+    acc.save()
+
+    return success_response(message='成功')
+
